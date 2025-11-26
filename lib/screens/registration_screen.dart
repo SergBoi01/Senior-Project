4a78c332f9aef0332f86488921168bf0c8a66636
Sergio Velazquez <sergio.velaz01444@gmail.com>
2025-11-25 23:34:33 -0600

    l

--- lib/screens/registration_screen.dart
+++ lib/screens/registration_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-
 class RegistrationScreen extends StatefulWidget {
   @override
   _RegistrationScreenState createState() => _RegistrationScreenState();
